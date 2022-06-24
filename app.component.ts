3407c41cdb3a91eeb5ff2749e80484bdeb01968e
Sagar <38463328+sagarkilana@users.noreply.github.com>
2022-06-24 13:46:37 +0530

    Create app.component.ts

---
+++ app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'Tour of Books';
+}
