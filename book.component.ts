27185ffabaa37cc6d0a9c13d8d7da05396be3728
Sagar <38463328+sagarkilana@users.noreply.github.com>
2022-06-24 13:48:19 +0530

    Create book.component.ts

---
+++ book.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from './book';
+import { BookService } from './book.service';
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
+  books!: Book[];
+  errorMessage!: string;  
+  constructor(private bookService: BookService) { }
+  getBooks() {
+    this.bookService.getBooks().subscribe({
+       next:  books => this.books = books,
+       error:error => this.errorMessage = <any>error
+     })
+   } 
+  
+  ngOnInit(): void {
+    this.getBooks();
+  }
+}
